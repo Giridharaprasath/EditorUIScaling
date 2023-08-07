8ad48bb4c6cdc5073dd22c80a2315b8c1849353d
Giridharaprasath M <girisagayam@gmail.com>
2023-08-07 21:34:31 +0530

    Added Files

---
+++ Source/EditorUIScaling/Private/MelonLordDeveloperSettings.cpp
@@ -0,0 +1,30 @@
+// Developed By Melon Studios.
+
+
+#include "MelonLordDeveloperSettings.h"
+
+UMelonLordDeveloperSettings::UMelonLordDeveloperSettings(const FObjectInitializer& obj) : EditorUIScale(1.0f)
+{
+	CategoryName = "General";
+	SectionName = "Melon Lord";
+}
+
+void UMelonLordDeveloperSettings::PostInitProperties()
+{
+	Super::PostInitProperties();
+
+	if (!IsRunningCommandlet())
+	{
+		FSlateApplication::Get().SetApplicationScale(EditorUIScale);
+	}
+}
+
+void UMelonLordDeveloperSettings::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	if (PropertyChangedEvent.GetPropertyName() == "EditorUIScale")
+	{
+		FSlateApplication::Get().SetApplicationScale(EditorUIScale);
+	}
+}
